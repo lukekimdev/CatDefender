2359de93b561c60d63346e0bde43a158dac12a07
Simina Pasat <siminap@microsoft.com>
2017-06-06 16:27:52 -0700

    Initial Commit

---
+++ CatDefender/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CatDefender
+//
+//  Created by REDMOND\siminap on 6/6/17.
+//  Copyright © 2017 Simina Pasat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
